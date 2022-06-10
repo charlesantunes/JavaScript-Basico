83f43d5805e178288e141c16cd2ce12ab7cccba2
charlesantunes <charleswcantunes@gmail.com>
2022-06-10 13:21:57 -0300

    criando src para icon e script

---
+++ script.js
@@ -0,0 +1,12 @@
+const nome = "Charles";
+        const numeroGuardado = 1999;
+        const anoAtual = 2022;
+        let resultSubtracao = anoAtual - numeroGuardado;
+        let resultAdicao = anoAtual + numeroGuardado;
+        let quartaFeira = false;
+        
+        console.log(`O resultado da subtração entre o número guardado ${anoAtual} e o ano atual ${numeroGuardado} é ${resultSubtracao}`);
+        console.log(`O resultado da adição entre o número guardado ${anoAtual} e o ano atual ${numeroGuardado} é ${resultAdicao}`);
+        console.log("Estou mexendo no console.log");
+        console.log(`Hoje é quarta feira? verdade ou falso? \n${quartaFeira}`);
+        console.log("O tipo de dado da váriavel quartaFeira é "+typeof(quartaFeira));
